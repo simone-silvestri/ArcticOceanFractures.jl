96b7221c5df04ae0065b8d70ae14c37c2633056e
Simone Silvestri <silvestri.simone0@gmail.com>
2025-05-14 12:56:09 +0200

    add halo

--- arctic_grid.jl
+++ arctic_grid.jl
@@ -6,6 +6,6 @@ z_faces = MutableVerticalDiscretization(r_faces)
 
 grid = TripolarGrid(arch; size=(Nx, Ny, Nz), z=z_faces, southernmost_latitude=40)
 
-bottom_height = regrid_bathymetry(grid; minimum_depth=20, major_basins=1)
+bottom_height = regrid_bathymetry(grid; minimum_depth=20, major_basins=1, halo=(7, 7, 7))
 
 grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height))
