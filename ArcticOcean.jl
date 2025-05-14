a9a92af6836d124a0b35e289f6501743fbbf49fb
simone-silvestri <silvestri.simone0@gmail.com>
2025-05-14 06:43:56 -0400

    commit

--- ArcticOcean.jl
+++ ArcticOcean.jl
@@ -5,12 +5,13 @@ using PythonCall
 using ClimaSeaIce
 using Oceananigans
 using Oceananigans.Grids
+using Oceananigans.Units
 using Dates
 
 const start_date = Date(1996, 11, 1)
 const end_date   = Date(1997, 06, 1)
 
-const arch = CPU()
+const arch = GPU()
 const Nx = 100# 4320
 const Ny = 10 #450
 const Nz = 10 #60
--- glorys_restoring.jl
+++ glorys_restoring.jl
@@ -3,11 +3,12 @@ using ClimaOcean
 using ClimaOcean.Copernicus
 using ClimaOcean.DataWrangling
 using Oceananigans
+using Oceananigans.OutputReaders
 
 bounding_box = DataWrangling.BoundingBox(longitude=(-180.0, 180.0), latitude=(35.0, 90.0), z=(-6000, 0))
 
-T_meta = Metadata(:temperature; dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="data/") 
-S_meta = Metadata(:salinity;    dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="data/") 
+T_meta = Metadata(:temperature; dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="./data/") 
+S_meta = Metadata(:salinity;    dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="./data/") 
 
 # Restore quite strongly between 40 and 42.5 north then decrease linearly between 42.5 and 45 north
 function mask(λ, φ, z, t) 
@@ -22,5 +23,5 @@ kwargs = (; rate = 1/10days, mask, time_indexing=Linear(), time_indices_in_memor
 RT = ClimaOcean.DatasetRestoring(T_meta, arch; kwargs...)
 RS = ClimaOcean.DatasetRestoring(S_meta, arch; kwargs...)
 
-SI_meta = Metadata(:sea_ice_concentration; dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="data/")
-SC_meta = Metadata(:sea_ice_concentration; dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="data/")
+SI_meta = Metadata(:sea_ice_concentration; dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="./data/")
+SC_meta = Metadata(:sea_ice_concentration; dataset=GLORYSDaily(), start_date, end_date, bounding_box, dir="./data/")
